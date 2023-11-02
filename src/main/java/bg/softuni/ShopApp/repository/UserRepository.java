aa06b0478291b72d66e575ed3c7cd71b9f9a6a29
Dimitar Simeonov <simeonov88@abv.bg>
2023-11-02 15:23:05 +0200

    Custom annotation update

--- src/main/java/bg/softuni/ShopApp/repository/UserRepository.java
+++ src/main/java/bg/softuni/ShopApp/repository/UserRepository.java
@@ -2,14 +2,18 @@ package bg.softuni.ShopApp.repository;
 
 import bg.softuni.ShopApp.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT u FROM User u WHERE u.username = :username")
     boolean existByUsername(String username);
 
+    @Query("SELECT u FROM User u WHERE u.email = :email")
     boolean existByEmail(String email);
 
+    @Query("SELECT u FROM User u WHERE u.phoneNumber = :phoneNumber")
     boolean existByPhoneNumber(String phoneNumber);
 }
---
+++ src/main/java/bg/softuni/ShopApp/web/UserController.java
@@ -0,0 +1,22 @@
+package bg.softuni.ShopApp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/login")
+    public String login() {
+
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+
+        return "register";
+    }
+}
