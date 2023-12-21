266c9f55c4ed19c088f8f483bce615dd7952e3ce
Dimitar Simeonov <simeonov88@abv.bg>
2023-12-21 15:51:42 +0200

    Create tests

---
+++ src/test/java/bg/softuni/shop_app/web/SearchControllerTest.java
@@ -0,0 +1,67 @@
+package bg.softuni.shop_app.web;
+
+import bg.softuni.shop_app.model.dto.product.ProductSearchDTO;
+import bg.softuni.shop_app.model.dto.product.ProductViewDTO;
+import bg.softuni.shop_app.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SearchControllerTest {
+
+    private SearchController searchControllerToTest;
+
+    @Mock
+    private ProductService mockProductService;
+
+    @BeforeEach
+    void setUp() {
+        searchControllerToTest = new SearchController(mockProductService);
+    }
+
+    @Test
+    void search() {
+        assertEquals("product-search", searchControllerToTest.search(mock(Model.class)));
+    }
+
+    @Test
+    void searchConfirm() {
+        ProductSearchDTO productSearchDTO = new ProductSearchDTO();
+        BindingResult bindingResult = mock(BindingResult.class);
+        RedirectAttributes redirectAttributes = mock(RedirectAttributes.class);
+        Model model = mock(Model.class);
+        List<ProductViewDTO> products = new ArrayList<>();
+
+        when(bindingResult.hasErrors())
+                .thenReturn(true);
+
+        assertEquals("redirect:search", searchControllerToTest.searchConfirm(productSearchDTO,
+                bindingResult,
+                redirectAttributes,
+                model));
+
+        when(bindingResult.hasErrors())
+                .thenReturn(false);
+
+        when(mockProductService.searchByInput(productSearchDTO))
+                .thenReturn(products);
+
+        assertEquals("product-search-result", searchControllerToTest.searchConfirm(productSearchDTO,
+                bindingResult,
+                redirectAttributes,
+                model));
+    }
+}
