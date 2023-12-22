aba4b63ad65b9996082fcfeef53254aeb791ca7b
Dimitar Simeonov <simeonov88@abv.bg>
2023-12-22 15:29:17 +0200

    Refactoring tests

--- src/main/java/bg/softuni/shop_app/web/HomeController.java
+++ src/main/java/bg/softuni/shop_app/web/HomeController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
 
@@ -27,10 +29,9 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public String home(Model model) {
+    public String home(Model model, Principal principal) {
 
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
+        String username = principal.getName();
 
         model.addAttribute("myOffers", userService.getMyOffers(username));
 
--- src/test/java/bg/softuni/shop_app/web/HomeControllerTest.java
+++ src/test/java/bg/softuni/shop_app/web/HomeControllerTest.java
@@ -11,11 +11,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.ui.Model;
 
+import java.security.Principal;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class HomeControllerTest {
@@ -42,8 +42,14 @@ class HomeControllerTest {
     @Test
     @WithMockUser(username = "username" ,roles = {"ADMIN", "USER"})
     void home() {
-        when(userService.getMyOffers("username"))
-                .thenReturn(new ArrayList<ProductHomePageViewDTO>());
-        assertEquals("home", homeControllerToTest.home(mock(Model.class)));
+        Model model = mock(Model.class);
+        Principal principal = mock(Principal.class);
+
+        when(principal.getName())
+                .thenReturn("username");
+
+        String username = principal.getName();
+        assertEquals("home", homeControllerToTest.home(model, principal));
+        verify(userService).getMyOffers(username);
     }
 }
